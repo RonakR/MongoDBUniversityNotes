b7df93998e367dda63e2dc0215d2ce51de128e94
Ronak Raithatha <ronaksraithatha@gmail.com>
2018-09-18 01:59:00 -0400

    notes from week 3

---
+++ Course Notes/Week3/Week3.md
@@ -0,0 +1,1 @@
+# Week 3: The Node.js Driver
--- week_3_the_node_js_driver/buildingQueryDocuments_569ef31dd8ca393add3abeba/buildingQueryDocuments/buildingQueryDocuments.js
+++ week_3_the_node_js_driver/buildingQueryDocuments_569ef31dd8ca393add3abeba/buildingQueryDocuments/buildingQueryDocuments.js
@@ -111,15 +111,14 @@ function queryDocument(options) {
     console.log(options);
     
     var query = {
-        "tag_list": /* TODO: Complete this statement to match the regular expression "social-networking" */        
+        "tag_list": { "$regex": "social-networking", "$options": "i"}
     };
 
     if (("firstYear" in options) && ("lastYear" in options)) {
-        /* 
-           TODO: Write one line of code to ensure that if both firstYear and lastYear 
-           appear in the options object, we will match documents that have a value for 
-           the "founded_year" field of companies documents in the correct range. 
-        */
+        query.founded_year = {
+            "$gte": options.firstYear,
+            "$lte": options.lastYear
+        }
     } else if ("firstYear" in options) {
         query.founded_year = { "$gte": options.firstYear };
     } else if ("lastYear" in options) {
@@ -133,6 +132,7 @@ function queryDocument(options) {
            is a nested document containing fields that describe a corporate office. Each office
            document contains a "city" field. A company may have multiple corporate offices. 
         */
+       query["offices.city"] = options.city;
     }
         
     return query;
